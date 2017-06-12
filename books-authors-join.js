8b96ae993073f16e7465ba1a9a30d202d793cbb9
Daniel Isham <danielisham1@gmail.com>
2017-06-12 15:30:02 -0500

    sample data added

---
+++ books-authors-join.js
@@ -0,0 +1,43 @@
+module.exports =
+[
+  {
+    ID: 1,
+    book_id: 1,
+    author_id: 1,
+  },
+  {
+    ID: 2,
+    book_id: 1,
+    author_id: 2,
+  },
+  {
+    ID: 3,
+    book_id: 1,
+    author_id: 3,
+  },
+  {
+    ID: 4,
+    book_id: 2,
+    author_id: 4,
+  },
+  {
+    ID: 5,
+    book_id: 3,
+    author_id: 5,
+  },
+  {
+    ID: 6,
+    book_id: 4,
+    author_id: 6,
+  },
+  {
+    ID: 7,
+    book_id: 5,
+    author_id: 6,
+  },
+  {
+    ID: 8,
+    book_id: 6,
+    author_id: 6,
+  },
+];
