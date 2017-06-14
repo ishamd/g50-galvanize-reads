a2b69cd879b19590fb9e75500c3c6992446e80be
Daniel Isham <danielisham1@gmail.com>
2017-06-14 15:10:22 -0500

    books route promise added

--- routes/books.js
+++ routes/books.js
@@ -13,12 +13,50 @@ app.set('view engine', 'ejs');
 
 // requests all books from our database
 router.get('/books', (_req, res, next) => {
-  knex('books')
-  .then((books) => {
-    res.render('books', {
-      books,
+  function getBook(id) {
+    return knex('books')
+    .where('books.id', id);
+  }
+
+  function getAuthorsForBook(bookId) {
+    return knex('authors')
+    .join('books_authors', 'authors.id', 'books_authors.author_id')
+    .where('books_authors.book_id', bookId);
+  }
+
+  function getBookWithAuthors(bookId) {
+    return Promise.all([
+      getBook(bookId),
+      getAuthorsForBook(bookId),
+    ]).then((results) => {
+      const [book, authors] = results;
+      book.authors = authors;
+      return book;
+    }).then((books) => {
+      res.render('books', {
+        books,
+      });
     });
-  })
+  }
+
+  return getBookWithAuthors(1)
+
+  // knex.from('books')
+  // .innerJoin('books_authors', 'books.id', 'books_authors.book_id')
+  // .innerJoin('authors', 'books_authors.author_id', 'authors.id')
+  // .then((books) => {
+  //   console.log(books);
+  //   res.render('books', {
+  //     books,
+  //   });
+  // })
+
+  // knex('books')
+  // .then((books) => {
+  //   res.render('books', {
+  //     books,
+  //   });
+  // })
   .catch((err) => {
     next(err);
   });
