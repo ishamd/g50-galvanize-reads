d7333f1bd9d80f2605c345c93302e6b15113ea17
Daniel Isham <danielisham1@gmail.com>
2017-06-23 15:20:41 -0500

    heroku db added to knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,10 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/reads_dev',
+    connection: 'postgres://localhost/reads_dev'
   },
+  production: {
+    client: 'pg',
+    connection: 'postgres://qogykodrjxqhls:2556d86d2dc4b66f1b2e9ca8ce686bfb51c55d52cca268743f18c038d73e5a2a@ec2-107-22-250-33.compute-1.amazonaws.com:5432/d97sbs337tn6d2'
+  }
 };
